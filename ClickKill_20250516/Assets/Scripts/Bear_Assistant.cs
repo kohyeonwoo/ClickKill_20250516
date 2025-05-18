f02d5581cf07ca2fffef2c06894a57f197b2cd58
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-05-19 01:03:42 +0900

    250519
    곰 스크립트 내용 전면 제거 및 수정, 공격용 오브젝트 추가 및 스크립트 적용

--- ClickKill_20250516/Assets/Scripts/Bear_Assistant.cs
+++ ClickKill_20250516/Assets/Scripts/Bear_Assistant.cs
@@ -12,24 +12,10 @@ public class Bear_Assistant : Assistant
 
     public Transform target;
 
-    public bool bAttack;
-
-    public float distanceToEnemy;
-
-    [SerializeField]
-    private float range = 7.5f;
+    public float range;
 
     [SerializeField]
     private string targetTag = "Enemy";
-
-    [SerializeField]
-    private float attackRange = 1.0f;
-
-    [SerializeField]
-    private float attackRate = 1.0f;
-
-    [SerializeField]
-    private float attackCoolDown = 0.0f;
 
     private void Start()
     {
@@ -47,50 +33,7 @@ public class Bear_Assistant : Assistant
         {
             return;
         }
-        else
-        {
-            anim.SetBool("bMove", false);
-
-
-            distanceToEnemy = Vector3.Distance(transform.position,
-            target.transform.position);
-
-            if (distanceToEnemy < attackRange)
-            { //목표 위치
-                Vector3 to = new Vector3(target.position.x, 0, target.position.z);
-                //내 위치
-                Vector3 from = new Vector3(transform.position.x, 0, transform.position.z);
-
-                //곧바로 목표를 향해 돌기
-                transform.rotation = Quaternion.LookRotation(to - from);
-
-                ////천천히 목표를 향해 돌기
-                //Quaternion rotation = Quaternion.LookRotation(to - from);
-                //transform.rotation = Quaternion.Slerp(transform.rotation, RotationDriveMode, 0.01f);
-
-                agent.isStopped = true;
-                Debug.Log("적이 공격 범위 내에 왔습니다");
-
-                if (attackCoolDown <= 0.0f)
-                {
-                    //anim.SetTrigger("Attack1");
-                    anim.SetBool("bAttack", true);
-                    attackCoolDown = 1.0f / attackRate;
-                }
-
-                attackCoolDown -= Time.deltaTime;
-
-
-            }
-            else
-            {
-                anim.SetBool("bAttack", false);
-                agent.isStopped = false;
-                agent.SetDestination(target.position);
-                anim.SetBool("bMove", true);
-            }
-
-        }
+       
     }
 
     public void ActiveAttackCollision()
@@ -133,15 +76,6 @@ public class Bear_Assistant : Assistant
             target = null;
         }
 
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, range);
-
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 
 }
