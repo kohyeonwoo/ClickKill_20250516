c002880ec42309053f4e0f58898a2275c07f5ca2
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-05-19 21:37:35 +0900

    250519
    새로운 플레이어 유닛 추가 및 기능 반영

---
+++ ClickKill_20250516/Assets/Scripts/Mimic_Assistant.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Mimic_Assistant : MonoBehaviour
+{
+    public Transform center;
+
+    private NavMeshAgent nav;
+
+    public float moveRange;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+        center = GameObject.FindGameObjectWithTag("Center").transform;
+    }
+
+    private void Update()
+    {
+        if (nav.remainingDistance <= nav.stoppingDistance)
+        {
+            Vector3 point;
+
+            if (RandomPoint(center.position, moveRange, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+
+                nav.SetDestination(point);
+            }
+
+        }
+    }
+
+
+    bool RandomPoint(Vector3 Center, float Range, out Vector3 Result)
+    {
+        Vector3 randomPoint = Center + Random.insideUnitSphere * Range;
+
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            Result = hit.position;
+
+            return true;
+        }
+
+        Result = Vector3.zero;
+
+        return false;
+    }
+
+}
